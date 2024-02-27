fe032f8e8a3c8cfb740caa92823c8c0224187eb4
Maha <maha@Mahamudas-MacBook-Pro.local>
2024-02-27 22:04:30 +0000

    Updated all the files, ran npm test and created a HTML file using the command line.

--- starter/index.js
+++ starter/index.js
@@ -5,8 +5,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// Rest of your code
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
 const render = require("./src/page-template.js");
 
--- starter/src/page-template.js
+++ starter/src/page-template.js
@@ -4,6 +4,7 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
+        <div class="flex-container">
         <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
@@ -23,7 +24,7 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
+        <div class="card employee-card" class="flex-item">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
@@ -42,7 +43,7 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
+        <div class="card employee-card" class="flex-item">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
@@ -93,11 +94,12 @@ module.exports = team => {
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="team.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
 <body>
+<header class="jumbotron">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
@@ -105,6 +107,7 @@ module.exports = team => {
             </div>
         </div>
     </div>
+    </header>
     <div class="container">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
